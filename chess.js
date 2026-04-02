cac9dfd751c7d6bf2f3289af0501fb40bc8e603c
Cat Vokey <caitlinvokey@hotmail.com>
2026-04-02 10:33:38 -0230

    Added chess boards/pieces and styling.

---
+++ chess.js
@@ -0,0 +1,19 @@
+// DO NOT USE ... AS VALID JS
+// const cell_id = "5_d";
+// const [row_str, column_str] = cell_id.split("_");
+// const board = {
+//   "1": {
+//     "a": {
+//          type: "queen",
+//          player: "w",
+//      },
+//     "b": "",
+//     ...
+//   },
+//   "2": ...,
+//   "3": ...,
+//   "4": ...,
+//   "5": ...,
+//   ...
+// }
+// board[row_str][column_str]
