9eb7369e7d9a16805a308e12d71ed3a330e0b16d
ViperLPM <liampat8@hotmail.com>
2026-04-14 10:07:52 -0230

    Fixed File Structure from first contribution

--- chess.js
+++ chess.js
@@ -35,10 +35,37 @@ const make_board = () => {
 
 const init_board = (board) => {
     update_cell("8_a", board, "rook", "b");
-    update_cell("8_a", board, "rook", "b");
-    update_cell("8_a", board, "rook", "b");
-    update_cell("8_a", board, "rook", "b");
-    update_cell("8_a", board, "rook", "b");
+    update_cell("8_b", board, "knight", "b");
+    update_cell("8_c", board, "bishop", "b");
+    update_cell("8_d", board, "queen", "b");
+    update_cell("8_e", board, "king", "b");
+    update_cell("8_f", board, "bishop", "b");
+    update_cell("8_g", board, "knight", "b");
+    update_cell("8_h", board, "rook", "w");
+    update_cell("7_a", board, "pawn", "w");
+    update_cell("7_b", board, "pawn", "w");
+    update_cell("7_c", board, "pawn", "w");
+    update_cell("7_d", board, "pawn", "w");
+    update_cell("7_e", board, "pawn", "w");
+    update_cell("7_f", board, "pawn", "w");
+    update_cell("7_g", board, "pawn", "w");
+    update_cell("7_h", board, "pawn", "w");
+    update_cell("1_a", board, "rook", "w");
+    update_cell("1_b", board, "knight", "w");
+    update_cell("1_c", board, "bishop", "w");
+    update_cell("1_d", board, "queen", "w");
+    update_cell("1_e", board, "king", "w");
+    update_cell("1_f", board, "bishop", "w");
+    update_cell("1_g", board, "knight", "w");
+    update_cell("1_h", board, "rook", "w");
+    update_cell("2_a", board, "pawn", "w");
+    update_cell("2_b", board, "pawn", "w");
+    update_cell("2_c", board, "pawn", "w");
+    update_cell("2_d", board, "pawn", "w");
+    update_cell("2_e", board, "pawn", "w");
+    update_cell("2_f", board, "pawn", "w");
+    update_cell("2_g", board, "pawn", "w");
+    update_cell("2_h", board, "pawn", "w");
 };
 
 const board = make_board();
